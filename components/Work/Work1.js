834dd5540a53e866f1e4c98a899a27202c4ad695
damkols <damolausmankolapo@gmail.com>
2022-04-11 13:34:33 -0800

    added new projects

--- components/Work/Work1.js
+++ components/Work/Work1.js
@@ -26,11 +26,12 @@ const Work1 = () => {
     },
 
     {
-      title: "Loopstudios",
-      tools: "React, SaSS",
-      description: "A Virtual reality website",
-      live: "https://dazzling-albattani-46fe4e.netlify.app/",
-      code: "https://github.com/Damkols/loopstudios",
+      title: "Movie App",
+      tools: "React",
+      description:
+        "Built a Movie App that collects Movie ratings, Images, and descriptions from an API",
+      live: "http://damkols.me/movies-app/",
+      code: "https://github.com/Damkols/movies-app/tree/master",
       id: 3,
     },
 
@@ -52,6 +53,52 @@ const Work1 = () => {
       live: "https://62012087d2501b31ffa388fd--lucid-euler-ba1802.netlify.app/",
       code: "https://github.com/Damkols/PWA-Weather-App",
       id: 5,
+    },
+
+    {
+      title: "Loopstudios",
+      tools: "React, SaSS",
+      description: "A Virtual reality website",
+      live: "https://dazzling-albattani-46fe4e.netlify.app/",
+      code: "https://github.com/Damkols/loopstudios",
+      id: 6,
+    },
+
+    {
+      title: "Kiddies Landing Page",
+      tools: "React, Styled-components",
+      description:
+        "A landing page for kiddies learning built with React and Styled components",
+      live: "https://hungry-borg-14bf26.netlify.app/",
+      code: "https://github.com/Damkols/SqrLearning",
+      id: 7,
+    },
+
+    {
+      title: "Snippet Landing Page",
+      tools: "React, SASS",
+      description: "A Snippet landing page using React and SASS",
+      live: "https://6214d2c2fa42c7babdad1a0f--boring-panini-6fc31b.netlify.app/",
+      code: "https://github.com/Damkols/Snippets",
+      id: 8,
+    },
+
+    {
+      title: "Animated Landing page",
+      tools: "HTML, SASS, Gsap, JavaScript ",
+      description: "Love Yours, Jcole inspired Landing page",
+      live: "https://jade-peony-58058d.netlify.app/",
+      code: "https://github.com/Damkols/scroll-animation-with-Gsap-and-smooth-scrollbar",
+      id: 9,
+    },
+
+    {
+      title: "Stack Landing page",
+      tools: "React, CSS",
+      description: "3d stack effect Landing page built with React and CSS",
+      live: "https://spiffy-narwhal-db7ddc.netlify.app/",
+      code: "https://github.com/Damkols/3d-Stack-effect",
+      id: 10,
     },
   ]);
 
